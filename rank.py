cb508fcc8ea32362eb41cb35b7acd33b9e980d81
dolfus61 <152016783+dolfus61@users.noreply.github.com>
2025-11-09 11:34:03 +0530

    Add files via upload

--- rank.py
+++ rank.py
@@ -218,9 +218,9 @@ elif word:
             if final_latex_expr.endswith("+"):
                 final_latex_expr = final_latex_expr[:-1]
             if counter_multiplier == 1:
+                if final_latex_expr.startswith("\left("):
+                        final_latex_expr = final_latex_expr[7:]
                 if multiplier == 1:
-                    # if final_latex_expr.startswith("\left(1"):
-                    #     final_latex_expr = final_latex_expr[7:]
                     if common_value > 1:
                         
                         final_latex_expr += fr"\frac{{{remaining}!}}{{{common_fact}}} = "
@@ -231,9 +231,12 @@ elif word:
                         final_latex_expr += fr"{subtotal}"
                 else:
                     if common_value > 1:
-                        
-                        final_latex_expr += fr"\frac{{{remaining}!}}{{{common_fact}}} = "
+                        final_latex_expr += fr" \times \frac{{{remaining}!}}{{{common_fact}}} = "
+                        final_latex_expr += fr"{multiplier} \times \frac{{{remaining}!}}{{{common_fact}}} = "
                         final_latex_expr += fr"{subtotal}"
+                        
+                        # final_latex_expr += fr"\frac{{{remaining}!}}{{{common_fact}}} = "
+                        # final_latex_expr += fr"{subtotal}"
                     else:
                         
                         final_latex_expr += fr"{remaining}! = "
